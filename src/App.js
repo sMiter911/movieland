3a09d41d167eb69e755e5d8d8106f7b42750ceb1
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-06-25 13:52:35 +0200

    Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const Person = () => {
+  return (
+    <>
+      <h1>Name: Wandu</h1>
+      <h2>Surname: Mathunjwa</h2>
+      <h3>Age: 34</h3>
+    </>
+  );
+};
+
 const App = () => {
   const name = "Wandu";
   const isNameShowing = true;
 
   return (
     <div className="App">
-      <h1>Hello, {2 + 2}!</h1>
-      {isNameShowing ? (
-        <>
-          <h1>{name}</h1>
-        </>
-      ) : (
-        <>
-          <h1>{"There is no name here"}</h1>
-        </>
-      )}
+      <Person />
     </div>
   );
 };
